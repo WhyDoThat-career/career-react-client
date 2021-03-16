0b8b4773a09f94a8360c25e0c0f65850f91ae84f
duwon1229 <endndnjs2@naver.com>
2021-03-16 18:25:08 +0900

    jenkinds test

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,44 @@ import React from "react";
 import axios from "axios";
 import "./App.css";
 
-const test_axios = () => {
+const test_axiosGet = () => {
+  // const axios = require("axios");
   axios
-    .get(
-      "http://api.openweathermap.org/data/2.5/air_pollution?lat=50&lon=50&appid=d48ea8ac906fdf668bee2e7a42183a5f",
-    )
+    .get("http://15.164.162.236:8888/test")
     .then(function (response) {
       console.log(response);
       return response;
     })
     .catch(function (error) {
       console.log("false");
-      return error;
+      return;
+    });
+};
+
+const test_axiosPost = () => {
+  axios
+    .post("/http://15.164.162.236:8888/test", {
+      firstName: "Fred",
+      lastName: "Flintstone",
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
     });
 };
 
 function App() {
-  const get_json = test_axios();
+  const get_json = axios.get("http://15.164.162.236:8888/test");
+
+  // const post_json = test_axiosPost();
+  console.log(get_json);
+  // console.log(post_json);
   return (
     <div className="App">
-      <h1>hi</h1>
+      <h1>hello</h1>
+      <button onClick={() => console.log(get_json)}>button</button>
     </div>
   );
 }
