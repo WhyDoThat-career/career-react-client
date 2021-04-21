96fef14f263ab4b5995924a3d01c98d28dfc9fca
duwon1229 <endndnjs2@naver.com>
2021-04-21 15:23:27 +0900

    app.css 수정(margin 0 auto) & footer 수정

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -4,17 +4,37 @@ import styled from "styled-components";
 const Footer = () => {
   return (
     <Cover>
-      <div className="footer">
-        <p>©WhyDoThat</p>
-        <p>제작자 : 정근영, 김태훈, 나두원</p>
-        <p>Email : whydothat.studio@gmail.com</p>
-      </div>
+      <footer>
+        <div className="footer-create">제작자: 정근영, 김태훈, 나두원</div>
+        <div className="footer-email">Email: whydothat.studio@gmail.com</div>
+        <div className="footer-copyright">
+          Copyrightⓒ 2021. WhyDoThat. All rights reserved.
+        </div>
+      </footer>
     </Cover>
   );
 };
 
 const Cover = styled.div`
-  background-color: #3498db;
+  footer {
+    border-top: 1px solid #e4e4e4;
+    background-color: #f8f9fa;
+    padding: 1rem 0;
+    margin: 1rem 0;
+  }
+  .footer-create {
+    font-weight: bold;
+    color: #545e6f;
+    margin: 0.3rem 0.5rem;
+  }
+  .footer-email {
+    color: #545e6f;
+    margin: 0 0.5rem;
+  }
+  .footer-copyright {
+    color: #545e6f;
+    margin: 1rem 0.5rem;
+  }
 `;
 
 export default Footer;
