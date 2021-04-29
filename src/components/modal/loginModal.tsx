c5fe98909530726e24ceeb6a34d714c10e3c61d9
duwon1229 <endndnjs2@naver.com>
2021-04-30 00:18:17 +0900

    login modal(css 적용x)

--- src/components/modal/loginModal.tsx
+++ src/components/modal/loginModal.tsx
@@ -2,16 +2,20 @@ import React from "react";
 import styled from "styled-components";
 import Modal from "@material-ui/core/Modal";
 import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
+import WdtCon from "components/img/wdtcon.png";
 
 function rand() {
   return Math.round(Math.random() * 20) - 10;
 }
 
 function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
   return {
-    top: { height: "50%" },
-    left: { width: "50%" },
-    transform: `translate(-{top}, -{left})`,
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
   };
 }
 
@@ -20,8 +24,9 @@ const useStyles = makeStyles((theme: Theme) =>
     paper: {
       position: "absolute",
       width: 400,
+      height: 600,
       backgroundColor: theme.palette.background.paper,
-      border: "2px solid #000",
+      border: "1px solid #000",
       boxShadow: theme.shadows[5],
       padding: theme.spacing(2, 4, 3),
     },
@@ -33,10 +38,30 @@ export function LoginModal() {
   const [modalStyle] = React.useState(getModalStyle);
   return (
     <div style={modalStyle} className={classes.paper}>
-      <h2 id="simple-modal-title">Text in a modal</h2>
-      <p id="simple-modal-description">
-        Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-      </p>
+      <header>
+        <button className="close">x</button>
+      </header>
+      <section>
+        <img src={WdtCon} alt="WhyDoThat logo"></img>
+        <div>Why Do That!</div>
+        <div>지금 채용공고를 확인하세요!</div>
+        <form>
+          <input
+            type="email"
+            className="form-email"
+            id="email"
+            placeholder="이메일을 입력하세요."
+          ></input>
+          <input
+            type="password"
+            className="form-password"
+            id="password"
+            placeholder="비밀 번호"
+          ></input>
+        </form>
+      </section>
     </div>
   );
 }
+
+const Cover = styled.div``;
